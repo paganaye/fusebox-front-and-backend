3c824eef7eb203ac960bb1e06b7302db1615a373
Pascal <pascal.ganaye@itron.com>
2020-05-31 23:57:12 +0200

    jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    ...tsjPreset.transform,
+  }
+  
+};
